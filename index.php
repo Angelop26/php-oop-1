4ecf1de9bfede001e1a7a95e64686e2da58bb334
Angelop26 <angeloprospero26@gmail.com>
2023-05-16 13:36:52 +0200

    milestone complete

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+class Movie{
+    public $title;
+    public $description;
+    public $genre;
+
+    function __construct($_title)
+    {
+        $this->title = $_title;
+    }
+
+    public function setGenre($_genre){
+        $this->genre[] =$_genre;
+    } 
+}
+
+$movie1 = new Movie('Bella ciao');
+$movie1->setGenre('drammatico');
+$movie2 = new Movie('La Mummia');
+$movie2->setGenre('horror');
+var_dump($movie1,$movie2);
